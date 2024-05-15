9b938a7d4cd83fc88f7520d36e25c70308acc37f
Crowdbotics <team@crowdbotics.com>
2024-05-15 00:32:05 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "twilight-night-47938",
+  url: "https://twilight-night-47938.botics.co",
+  api: "https://twilight-night-47938.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
